3e9a5566ccebcbd14bdbceafb2b636761f091497
Swamiyogi <admin@swamiyogi.com>
2025-06-08 09:49:17 +0000

    modified:   src/utils/Cashfree.jsx
    Changes not staged for commit:
    	modified:   src/utils/Cashfree.jsx

--- src/utils/Cashfree.jsx
+++ src/utils/Cashfree.jsx
@@ -1,10 +1,17 @@
+// import { load } from "@cashfreepayments/cashfree-js";
+
+// async function initializeCashfree() {
+//   const cashfree = await load({
+//     mode: "production",
+//   });
+
+//   return cashfree;
+// }
+// export default initializeCashfree;
+
 import { load } from "@cashfreepayments/cashfree-js";
+const cashfree = await load({
+	mode: "sandbox" //or production
+});
 
-async function initializeCashfree() {
-  const cashfree = await load({
-    mode: "production",
-  });
-
-  return cashfree;
-}
-export default initializeCashfree;
+export default cashfree;
