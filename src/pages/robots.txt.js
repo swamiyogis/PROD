757acc9c10ebe3fb6b29fed59f86d1eeae65399b
Shorya Dwivedi <shoryadubey33@gmail.com>
2025-06-23 14:16:07 +0000

    new file:   src/pages/robots.txt 	deleted:    src/pages/robots.txt.js

--- src/pages/robots.txt.js
+++
@@ -1,9 +0,0 @@
-// src/pages/robots.txt.js
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(`User-agent: *
-Allow: /
-Disallow: /mysessions
-
-Sitemap: https://swamiyogi.com/sitemap.xml`);
-}
