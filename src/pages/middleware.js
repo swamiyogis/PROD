7f94df4a3c6424ec4d001ed7fb7f2e2fe699cdd2
Shorya Dwivedi <shoryadubey33@gmail.com>
2025-06-23 14:12:18 +0000

    deleted:    src/pages/middleware.js

--- src/pages/middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware(request) {
-  const response = NextResponse.next()
-  response.headers.set('x-robots-tag', 'index, follow')
-  return response
-}
