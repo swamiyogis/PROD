ef347eb69cb905aacb46d9af49d35bce0066c63e
Swamiyogi <admin@swamiyogi.com>
2025-07-17 21:29:12 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-    ];
-  },
-
-  async headers() {
-    if (process.env.NODE_ENV !== 'production') {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'x-robots-tag',
-              value: '', // Removes x-robots-tag in dev
-            },
-          ],
-        },
-      ];
-    }
-
-    return [];
-  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
